db6a5e579ffc616c11a01d84550acbfc77bc9ae5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-11 17:08:24 +1200

    Building first custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  // imperative JS approach, cumbersome
+  // const para = document.createElement("p");
+  // para.textContent = "This is also visible";
+  // document.getElementById("root").append(para);
   return (
+    // simplify using JSX (lower case element are build in html element while upper case are define by dev)
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
