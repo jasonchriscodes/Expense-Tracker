f7d0d4f343ef4ad207d2af865eb64d20fc00f034
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-11 17:27:21 +1200

    Writing more complex JSX code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  // only have 1 root element per JSX code (return element)
+  // solve it by wrap it all in one div
+  return (
+    <div>
+      <div>June 11th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
