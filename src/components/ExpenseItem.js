a54c1cd598c22e0b4e6cefdf6860c702af021b09
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-11 19:35:46 +1200

    Working dynamic data and expression in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 6, 11);
+  const expenseTitle = "Car insurance";
+  const expenseAmount = 294.67;
+
   // only have 1 root element per JSX code (return element)
   // solve it by wrap it all in one div
+  // toISOstring() change object to string
   return (
     <div className="expense-item">
-      <div>June 11th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
