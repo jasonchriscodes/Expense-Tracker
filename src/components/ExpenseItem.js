c006b0f99861d065065b13586bf129e7dd3fc0ba
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-13 00:33:52 +1200

    Adding composition (child props)
    using reusable wrapper component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   // only have 1 root element per JSX code (return element)
   // solve it by wrap it all in one div
   // toISOstring() change object to string
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
