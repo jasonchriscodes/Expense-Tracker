7a4c0078718922c3c587114bf913d84960a2db0a
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-12 22:31:08 +1200

    Splitting component into multiple components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" }); // 2-digit is the number of digits to show
+  const month = props.date.toLocaleString("en-US", { month: "long" }); // long is full name
+  const year = props.date.getFullYear(); // getFullYear() returns the year as a four digit number
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
