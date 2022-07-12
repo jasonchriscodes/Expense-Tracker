9cffea42d440af33839ad1ebd647c14ecb050f93
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-12 17:30:27 +1200

    Adding normal Javascript logic to components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" }); // 2-digit is the number of digits to show
+  const month = props.date.toLocaleString("en-US", { month: "long" }); // long is full name
+  const year = props.date.getFullYear(); // getFullYear() returns the year as a four digit number
   // only have 1 root element per JSX code (return element)
   // solve it by wrap it all in one div
   // toISOstring() change object to string
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
