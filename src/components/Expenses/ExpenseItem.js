55e7a736e2116af047351dbb56457d2a441978f5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-13 19:28:28 +1200

    Working with event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  // function clickHandler(){
+  //   console.log("Clicked!!!");
+  // }
+
+  //OR
+
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
+
   // only have 1 root element per JSX code (return element)
   // solve it by wrap it all in one div
   // toISOstring() change object to string
@@ -14,8 +24,9 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
