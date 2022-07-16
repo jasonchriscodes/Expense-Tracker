983a97086954b48786ec88d5b6648c8edd0efef5
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-16 16:56:02 +1200

    Updating state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,37 +2,45 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState(""); // listen always a string, even if we enter a number
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState(""); // listen always a string, even if we enter a number
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value); // stored state
-    setUserInput({
-      ...userInput, // copy all the other properties
-      enteredTitle: event.target.value, // change only enteredTitle
-    });
+    setEnteredTitle(event.target.value); // stored state
+    // setUserInput({
+    //   ...userInput, // copy all the other properties
+    //   enteredTitle: event.target.value, // change only enteredTitle
+    // });
+    // use this approach if schedule a lot of state updates at the same time, you could be depending on an outdated or incorrect state snapshot if use above approach. This approach will guarantee that the state snapshot it will always be the latest state snapshot and keeping all scheduled state updates in mind (safer way to ensure always the latest state snapshot)
+    // Always use this approach whenever state update depends on the previous state.
+    // setUserInput((prevState) => {
+      // return {
+      //   ...prevState,
+      //   enteredTitle: event.target.value,
+      // };
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput, // copy all the other properties
-      enteredAmount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput, // copy all the other properties
+    //   enteredAmount: event.target.value,
+    // });
   };
 
   const dateChageHandler = (event) => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput, // copy all the other properties
-      enteredDate: event.target.value,
-    });
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput, // copy all the other properties
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
