49f76c813ad2c2fd836072da7016ece1c1250342
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-07-17 01:28:44 +1200

    Controlled vs Uncontrolled & Stateless vs Stateful component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,40 +1,15 @@
-import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  /**
-   * State is used if we have data that changes should be reflected on the user interface
-   * Use state return 2 values:
-   * 1. the current value of the state (title)
-   * 2. a function to update the state (setTitle)
-   */
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
-
-  // function clickHandler(){
-  //   console.log("Clicked!!!");
-  // }
-
-  //OR
-
-  const clickHandler = () => {
-    setTitle("Updated!!!");
-    console.log(title);
-  };
-
-  // only have 1 root element per JSX code (return element)
-  // solve it by wrap it all in one div
-  // toISOstring() change object to string
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
